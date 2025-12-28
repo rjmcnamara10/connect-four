02851c299a9b06e50b60479c8bd29c4f4a4ee497
rjmcnamara10 <rjmcnamara10@gmail.com>
2025-12-28 17:58:38 -0500

    done with DropPiece and Print for a Board, starting CheckWinner

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"strings"
 )
 
 // connect four idea:
@@ -20,27 +20,139 @@ const (
 	Yellow
 )
 
+func (s Space) ToString() string {
+	switch s {
+	case Red:
+		return "\033[31m●\033[0m"
+	case Yellow:
+		return "\033[33m●\033[0m"
+	case Empty:
+		return "·"
+	default:
+		return "?"
+	}
+}
+
 type Board struct {
 	ToWin int
 	Grid  [][]Space
 }
 
+func (b *Board) DropPiece(piece Space, col int) error {
+	if piece == Empty {
+		return errors.New("attempting to drop empty space")
+	}
+	if col < 0 || col >= len(b.Grid) {
+		return errors.New("column out of bounds")
+	}
+
+	for row, space := range b.Grid[col] {
+		if space == Empty {
+			b.Grid[col][row] = piece
+			return nil
+		}
+	}
+	return errors.New("column is full")
+}
+
+func (b *Board) Print() {
+	rowCount := len(b.Grid[0])
+	colCount := len(b.Grid)
+	for row := rowCount - 1; row >= 0; row-- {
+		for col := range colCount {
+			fmt.Print(b.Grid[col][row].ToString())
+			if col == colCount-1 {
+				fmt.Println()
+			} else {
+				fmt.Print(" ")
+			}
+		}
+	}
+}
+
+func (b *Board) CheckWinner() Space {
+	// Check horizontal
+	rowCount := len(b.Grid[0])
+	colCount := len(b.Grid)
+	for row := range rowCount {
+		curStreak := 0
+		streakSpace := Empty
+		for col := range colCount {
+			curSpace := b.Grid[col][row]
+			switch curSpace {
+			case Empty:
+				curStreak = 0
+				streakSpace = Empty
+			case streakSpace:
+				curStreak++
+				// Check if player has enough spaces in a row to win
+				if curStreak >= b.ToWin {
+					return streakSpace
+				}
+			default:
+				curStreak = 1
+				streakSpace = curSpace
+			}
+		}
+	}
+
+	// For diagonal check, first check if possible to get b.ToWin in a row diagonally
+	// (also do this for vertical/horizontal?)
+
+	// If no winner, return Empty
+	return Empty
+}
+
 func main() {
-	// Create a tic-tac-toe board.
-	board := [][]string{
-		{"_", "_", "_"},
-		{"_", "_", "_"},
-		{"_", "_", "_"},
+	rowCount := 6
+	colCount := 7
+	toWin := 4
+
+	grid := make([][]Space, colCount)
+	for col := range grid {
+		grid[col] = make([]Space, rowCount)
 	}
 
-	// The players take turns.
-	board[0][0] = "X"
-	board[2][2] = "O"
-	board[1][2] = "X"
-	board[1][0] = "O"
-	board[0][2] = "X"
-
-	for _, x := range board {
-		fmt.Printf("%s\n", strings.Join(x, " "))
+	board := Board{
+		ToWin: toWin,
+		Grid:  grid,
 	}
+
+	board.Print()
+	fmt.Println("-------------")
+	board.DropPiece(Red, 3)
+	board.Print()
+	fmt.Println("-------------")
+	board.DropPiece(Yellow, 2)
+	board.Print()
+	fmt.Println("-------------")
+	board.DropPiece(Red, 2)
+	board.Print()
+	fmt.Println("-------------")
+	board.DropPiece(Yellow, 0)
+	board.DropPiece(Yellow, 0)
+	board.DropPiece(Yellow, 0)
+	board.DropPiece(Yellow, 0)
+	board.DropPiece(Yellow, 0)
+	board.DropPiece(Yellow, 0)
+	board.DropPiece(Yellow, 0)
+	board.DropPiece(Yellow, 0)
+	if err := board.DropPiece(Yellow, 0); err != nil {
+		fmt.Println("Error:", err)
+	}
+	board.Print()
+
+	// fmt.Print("\033[31m●\033[0m")
+	// fmt.Print("\033[33m●\033[0m")
+	// fmt.Println("\033[31m●\033[0m")
+	// fmt.Print("\033[33m●\033[0m")
+	// fmt.Print("\033[31m●\033[0m")
+	// fmt.Println("\033[33m●\033[0m")
+	// // fmt.Println("\033[33m⬤\033[0m")
+	// fmt.Print("\033[31mO\033[0m")
+	// fmt.Print("\033[33mO\033[0m")
+	// fmt.Println("\033[31mO\033[0m")
+	// fmt.Print("\033[33mO\033[0m")
+	// fmt.Print("\033[31mO\033[0m")
+	// fmt.Println("\033[33mO\033[0m")
 }
